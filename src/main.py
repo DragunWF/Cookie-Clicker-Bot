48d10e23b57de85b219d13582d7d7077cf5e3a36
DragonWF <plarisanmarc@gmail.com>
2022-03-22 02:27:11 +0800

    Added main functions

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+
+from bot import Bot
+from utils import Utils
+
+
+def user_choose():
+    pass
+
+
+def configuration():
+    pass
+
+
+def main():
+    bot = Bot()
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
