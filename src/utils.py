9f73a503dfaa802d862d821959a27c1898489b0e
DragonWF <plarisanmarc@gmail.com>
2022-03-22 02:26:55 +0800

    Added main utility functions

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import pyttsx3
+from colored import fg
+
+voice_engine = pyttsx3.init("sapi5")
+voices = voice_engine.getProperty("voices")
+voice_engine.setProperty("voice", voices[0].id)
+
+
+class Utils:
+    @staticmethod
+    def text_to_speech(text):
+        voice_engine.say(text)
+        voice_engine.runAndWait()
+
+    @staticmethod
+    def colored_print(text, color):
+        color_func = fg(f"light_{color}") if color != "white" else fg("white")
+        print(color_func + text)
+
+    @staticmethod
+    def tts_print(text, color=None):
+        Utils.colored_print(text, color if color else "white")
+        Utils.text_to_speech(text)
