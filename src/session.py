dc342e9a32cbdeac00d62165caed0edd6885d0fa
DragonWF <plarisanmarc@gmail.com>
2022-03-25 01:24:09 +0800

    Implemented more instance variables

--- src/session.py
+++ src/session.py
@@ -7,15 +7,21 @@ class Session:
         self.cookies_from_start = 0
         self.cookies_at_end = 0
         self.cookies_gained = 0
+        self.upgrades_bought = 0
+
         self.save_file_loaded = None
+        self.result = None
 
     def save_session(self):
         data = json.loads(Path("data/session.json").read_text())
 
-        session_data = {"cookies_from_start": self.cookies_from_start,
+        result = "Success" if self.result else "Error"
+        session_data = {"cookies_gained": self.cookies_gained,
+                        "cookies_from_start": self.cookies_from_start,
                         "cookies_at_end": self.cookies_at_end,
-                        "cookies_gained": self.cookies_gained,
-                        "save_file_loaded": self.save_file_loaded}
+                        "upgrades_bought": self.upgrades_bought,
+                        "save_file_loaded": self.save_file_loaded,
+                        "end_result": result}
         data.append(session_data)
 
         formatted = json.dumps(data, sort_keys=False,
