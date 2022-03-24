983e7433d286280fac85135b54456623b64d8ac6
DragonWF <plarisanmarc@gmail.com>
2022-03-25 01:27:35 +0800

    Added more variables at main() and added logic for user_choose()

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 import json
+import os
 from pathlib import Path
+from colored import fg
 
 from bot import Bot
+from session import Session
 from utils import Utils
 
 
-def user_choose():
-    pass
+def user_choose(question: str) -> bool:
+    while True:
+        option = input(fg("light_green") +
+                       f"{question} (y/n) ").strip().lower()
+        if option == "y" or option == "yes":
+            return True
+        if option == "n" or option == "no":
+            return False
+        Utils.colored_print("Invalid option!", color="red")
 
 
 def configuration():
@@ -14,7 +24,12 @@ def configuration():
 
 
 def main():
-    bot = Bot()
+    root_directory = os.path.dirname(os.path.abspath(__file__))
+    settings = json.loads(Path("data/settings.json").read_text())[0]
+
+    session = Session()
+    bot = Bot(session, settings)
+    Utils.tts_print("Bot is now running!", color="green")
     bot.run()
 
 
