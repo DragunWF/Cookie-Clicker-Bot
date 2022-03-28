c97b70e20def660909e24acfa451ca6ad94af7c4
DragonWF <plarisanmarc@gmail.com>
2022-03-28 18:49:55 +0800

    Added functionality for controller and fixed root_dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import asyncio
 import json
 import os
 from pathlib import Path
 from colored import fg
 
 from bot import Bot
+from controller import BotController
 from session import Session
 from utils import Utils
 
@@ -24,13 +26,22 @@ def configuration():
 
 
 def main():
-    root_directory = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    while "\\" in root_dir:
+        root_dir = root_dir.replace("\\", "/")
+
     settings = json.loads(Path("data/settings.json").read_text())[0]
+    settings["save_file_location"] = f"{root_dir}/{settings['saves']['location']}"
 
     session = Session()
-    bot = Bot(session, settings)
-    Utils.tts_print("Bot is now running!", color="green")
-    bot.run()
+    Utils.tts_print("Session has been created!", color="green")
+
+    if settings["controlled"]:
+        bot = BotController(session, settings)
+        asyncio.run(bot.input_loop())
+    else:
+        bot = Bot(session, settings)
+        asyncio.run(bot.run())
 
 
 if __name__ == "__main__":
