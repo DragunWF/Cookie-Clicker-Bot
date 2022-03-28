5737f5aac48256937b1048f4530740ad4bc6ba05
DragonWF <plarisanmarc@gmail.com>
2022-03-28 19:48:55 +0800

    Added functionality for quitting the game with a key

--- src/controller.py
+++ src/controller.py
@@ -1,29 +1,45 @@
 import asyncio
 import keyboard
+from sys import exit
 
 from bot import Bot
 from utils import Utils
 
 
 class BotController(Bot):
-    def __init__(self):
+    def __init__(self, session, settings):
+        super().__init__(session, settings)
         self.automation_running = True
+        self.controls = settings["controls"]
+
         Utils.tts_print("Bot Controller has been initialized!", color="green")
+        asyncio.run(self.run())
 
     async def input_loop(self):
         while True:
-            if keyboard.is_pressed("num 1"):
+            if keyboard.is_pressed(self.controls["start_automation"]):
                 if not self.automation_running:
                     self.automation_running = True
                     Utils.tts_print("Automation on", color="green")
                 else:
-                    Utils.colored_print("Automation is already on", color="red")
-            elif keyboard.is_pressed("num 2"):
+                    Utils.tts_print("Automation is already on",
+                                    color="red")
+            elif keyboard.is_pressed(self.controls["stop_automation"]):
                 if self.automation_running:
                     self.automation_running = False
                     Utils.tts_print("Automation off", color="yellow")
                 else:
-                    Utils.colored_print("Automation is already off", color="red")
+                    Utils.tts_print("Automation is already off",
+                                    color="red")
+            elif keyboard.is_pressed(self.controls["quit_game"]):
+                Utils.tts_print("Stopping automation", color="yellow")
+                self.automation_running = False
+                self.grab_stats(True)
+                self.save_game()
+                self.session.save_session()
+                Utils.tts_print("Game has been saved!", color="green")
+                Utils.tts_print("Quitting Game", color="cyan")
+                exit()
 
             if self.automation_running:
-                asyncio.run(self.run())
+                await self.actions()
